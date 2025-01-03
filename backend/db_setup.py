1fddf7184b4f302732897d157ae6816c9379b418
Pravat Nagarkoti <prbt333@gmail.com>
2025-01-03 21:14:45 +0545

    logger script implemented in db methods

--- backend/db_setup.py
+++ backend/db_setup.py
@@ -1,5 +1,8 @@
 import mysql.connector
 from contextlib import contextmanager
+from logger import setup_logger
+
+logger = setup_logger('db_setup')
 
 @contextmanager
 def get_db_cursor(commit=False):
@@ -18,12 +21,14 @@ def get_db_cursor(commit=False):
     conn.close()
 
 def fetch_expenses_for_date(expense_date, table_name="expenses"):
+    logger.info(f"fetch_expenses_for_date called with {expense_date}")
     with get_db_cursor() as cursor:
         cursor.execute(f'SELECT * FROM {table_name} WHERE expense_date = %s', (expense_date,))
         expenses = cursor.fetchall()
         return expenses
 
 def insert_expense(expense_date, amount, category, notes, table_name="expenses"):
+    logger.info(f"insert_expense called with date: {expense_date}, amount: {amount}, category: {category}, notes: {notes}")
     with get_db_cursor(commit=True) as cursor:
         cursor.execute(
             f'INSERT INTO {table_name} (expense_date, amount, category, notes) VALUES (%s, %s, %s, %s)',
@@ -31,10 +36,12 @@ def insert_expense(expense_date, amount, category, notes, table_name="expenses")
         )
 
 def delete_expenses_for_date(expense_date, table_name="expenses"):
+    logger.info(f"delete_expenses_for_date called with {expense_date}")
     with get_db_cursor(commit=True) as cursor:
         cursor.execute(f'DELETE FROM {table_name} WHERE expense_date = %s', (expense_date,))
 
 def fetch_expense_summary(start_date, end_date, table_name="expenses"):
+    logger.info(f"fetch_expense_summary called with start: {start_date} end: {end_date}")
     with get_db_cursor() as cursor:
         cursor.execute(
             f'''
